10d14f56ea9662e5282b1af30fe8eab67bd18928
Bruno Santos <brunosantos@protonmail.com>
2018-08-14 06:49:44 -0300

    update

--- Capítulo4/Programa.java
+++ Capítulo4/Programa.java
@@ -2,16 +2,32 @@ class Conta {
     int numero;
     String titular;
     double saldo;
+
+    void saca(double quantidade) {
+        double novoSaldo = this.saldo - quantidade;
+        this.saldo = novoSaldo;
+    }
+
+    void deposita(double quantidade) {
+        this.saldo += quantidade;
+    }
 }
 
 class Programa {
     public static void main(String[] args) {
+        // criando a conta
         Conta minhaConta;
         minhaConta = new Conta();
 
+        // alterando os valores de minhaConta
         minhaConta.titular = "Duke";
         minhaConta.saldo = 1000.0;
 
+        //saca 200 reais
+        minhaConta.saca(200);
+
+        // deposita 500 reais
+        minhaConta.deposita(500);
         System.out.println("Saldo atual: " + minhaConta.saldo);
     }
 }
