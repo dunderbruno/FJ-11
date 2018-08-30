373d16f7c89d3dfe2e99266073c8de69d3c16ccf
Bruno Santos <brunosantos@protonmail.com>
2018-08-30 09:15:32 -0300

    class gerente

--- Capítulo9/banco.java
+++ Capítulo9/banco.java
@@ -4,3 +4,21 @@ public class Funcionario {
     private double salario;
     // métodos devem vir aqui
 }
+
+public class Gerente {
+    private String nome;
+    private String cpf;
+    private double salario;
+    private int senha;
+    private int numeroDeFuncionariosGerenciados;
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            System.out.println("Acesso Permitido!");
+            return true;
+        } else {
+            System.out.println("Acesso Negado!");
+            return false;
+        }
+    }
+    // outros métodos.
+}
